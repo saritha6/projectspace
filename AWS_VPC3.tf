086c02c651e628e88c683024ffe2545df11666ba
saritha6 <84068893+saritha6@users.noreply.github.com>
2023-08-09 11:07:32 +0530

    Create AWS_VPC3.tf

---
+++ AWS_VPC3.tf
@@ -0,0 +1,21 @@
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+resource "aws_route_table" "my_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+resource "aws_route_table_association" "my_rta" {
+  subnet_id = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_rt.id
+}
